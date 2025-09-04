337b695df81a1e8d9aac3155491c7c5c3aff4cc3
Marre-t-eau <fessler.mathieu@gmail.com>
2025-09-04 20:42:25 +0200

    Adding a checkbox to set or not the control of power

--- VelomMonoGame/VelomMonoGame.Core/Sources/Pages/ControlGamePage.cs
+++ VelomMonoGame/VelomMonoGame.Core/Sources/Pages/ControlGamePage.cs
@@ -8,11 +8,15 @@ internal class ControlGamePage : GamePage
 {
     ushort ActualTargetPower { get; set; } = 100;
     Text ActualTargetPowerText { get; set; }
+    private Checkbox ControlPowerCheckbox { get; set; }
 
     internal ControlGamePage(Game game, Vector2 size, IBluetoothManager bluetoothManager) : base(game, size, bluetoothManager)
     {
         PrepareControl();
-        bluetoothManager.StartControllingPower().Wait();
+        if (ControlPowerCheckbox.IsChecked)
+        {
+            bluetoothManager.StartControllingPower().Wait();
+        }
     }
 
     private async void PrepareControl()
@@ -20,13 +24,20 @@ internal class ControlGamePage : GamePage
         // Buttons to control the power of the device
         if (BluetoothManager.CanSetPower)
         {
+            // Create checkbox for controlling power
+            ControlPowerCheckbox = Checkbox.CreateCheckbox("Control power", true, OnPowerControlChanged);
+            ControlPowerCheckbox.Position = new Vector2(marge, Size.Y - ControlPowerCheckbox.Size.Y - marge);
+            Elements.Add(ControlPowerCheckbox);
+
             // By default we set the power at 100
             await BluetoothManager.SetPower(ActualTargetPower);
             await BluetoothManager.StartControllingPower();
+
             // Add 2 buttons to control the power
             Button increasePowerButton = Button.CreateButtonWithText("+", Color.White, Color.Purple, IncreaseTargetPower);
             increasePowerButton.Position = new Vector2(marge, marge);
             Elements.Add(increasePowerButton);
+
             ActualTargetPowerText = new Text
             {
                 Position = new Vector2(marge, increasePowerButton.Position.Y + increasePowerButton.Size.Y + marge),
@@ -34,9 +45,23 @@ internal class ControlGamePage : GamePage
                 Color = Color.DarkGray
             };
             Elements.Add(ActualTargetPowerText);
+
             Button decreasePowerButton = Button.CreateButtonWithText("-", Color.White, Color.Purple, DecreaseTargetPower);
             decreasePowerButton.Position = new Vector2(marge, ActualTargetPowerText.Position.Y + ActualTargetPowerText.Size.Y + marge);
             Elements.Add(decreasePowerButton);
+        }
+    }
+
+    private async void OnPowerControlChanged(bool isChecked)
+    {
+        if (isChecked)
+        {
+            await BluetoothManager.StartControllingPower();
+            await BluetoothManager.SetPower(ActualTargetPower);
+        }
+        else
+        {
+            await BluetoothManager.StopControllingPower();
         }
     }
 
@@ -44,13 +69,19 @@ internal class ControlGamePage : GamePage
     {
         ActualTargetPower += 5;
         ActualTargetPowerText.TextContent = ActualTargetPower.ToString();
-        await BluetoothManager.SetPower(ActualTargetPower);
+        if (ControlPowerCheckbox.IsChecked)
+        {
+            await BluetoothManager.SetPower(ActualTargetPower);
+        }
     }
 
     private async void DecreaseTargetPower()
     {
         ActualTargetPower -= 5;
         ActualTargetPowerText.TextContent = ActualTargetPower.ToString();
-        await BluetoothManager.SetPower(ActualTargetPower);
+        if (ControlPowerCheckbox.IsChecked)
+        {
+            await BluetoothManager.SetPower(ActualTargetPower);
+        }
     }
 }
