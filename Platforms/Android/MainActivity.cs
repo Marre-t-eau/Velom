3795c6890c44613d5ec1371756c63454f5edf98a
Marre-t-eau <fessler.mathieu@gmail.com>
2026-01-24 21:11:05 +0100

    Updating the logo, adding the tittle of the application and correct for execution

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,7 +7,7 @@ using AndroidX.Core.Content;
 
 namespace Velom
 {
-    [Activity(Theme = "@style/MainTheme", MainLauncher = true, Label = "", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         private const int REQUEST_PERMISSION_CODE = 1001;
